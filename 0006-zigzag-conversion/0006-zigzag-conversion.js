1218395358997c1424c77937fe2d062e1c720e80
Mr Sagar <154483781+sagarkuranagi@users.noreply.github.com>
2026-08-18 11:18:51 +0530

    Time: 8 ms (34.48%), Space: 56.1 MB (93.2%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+ if(numRows === 1|| numRows >= s.length){
+    return s;
+ }
+
+ let rows = new Array(numRows).fill("");
+ let currentRow =0;
+ let direction = 1;
+
+ for(let char of s){
+    rows[currentRow] += char;
+    if(currentRow === 0){
+        direction = 1;
+    }
+    else if(currentRow === numRows -1){
+        direction = -1;
+    }
+
+    currentRow +=direction;
+ }
+ return rows.join("");
+};
