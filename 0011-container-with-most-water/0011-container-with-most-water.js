0e6e3bd22dff476da03f8c03025e3b019112cf10
Mr Sagar <154483781+sagarkuranagi@users.noreply.github.com>
2026-08-21 16:48:24 +0530

    Time: 3 ms (48.86%), Space: 62.4 MB (78.53%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+
+    let max = 0;
+
+    while (left < right) {
+        const width = right - left;
+
+        const h = Math.min(
+            height[left],
+            height[right]
+        );
+
+        const area = width * h;
+
+        max = Math.max(max, area);
+
+        // Move the shorter line
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+
+    return max;
+};
